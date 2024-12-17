c8dd76e74c1943d8295bf137a659ce3c568ddc76
MangoKo4ek <habarovila76@gmail.com>
2024-12-17 10:14:22 +0300

    Update parsing.py
    Photo parse add

--- MathBot/back/parsing.py
+++ MathBot/back/parsing.py
@@ -12,3 +12,14 @@ async def parse(message: Message, bot):
     print("Сообщение получено!")
     await bot.send_message(chat_id=ADMIN_ID, text=message.text)
     await bot.send_message(chat_id=GROUP_ID, text=message.text)
+
+
+@parse_router.message(F.photo & F.text.regexp('(?i)Задача.+'))
+async def parse_image_text(message: Message, bot):
+    print("Сообщение с изображением и текстом получено!")
+    
+    text = message.caption if message.caption else "Без текста"
+    
+    await bot.send_photo(chat_id=ADMIN_ID, photo=message.photo[-1].file_id, caption=text)
+ 
+    await bot.send_photo(chat_id=GROUP_ID, photo=message.photo[-1].file_id, caption=text)
