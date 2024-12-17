c11d8ac92223744171666d029816a04a097c56f0
MangoKo4ek <habarovila76@gmail.com>
2024-12-17 12:11:53 +0300

    выф

--- MathBot/config.py
+++ MathBot/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path='C:/Users/user/Documents/GitHub/MathBot.project/.env')
 DB_PATH = os.getenv('DB')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 GROUP_ID = os.getenv('GROUP_ID')
